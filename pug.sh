1336a3c3b1f73289d6250c05617b8f86e27fd410
Aidan Jennings <anjennings@wisc.edu>
2020-08-27 11:11:27 -0500

    removed old html stuff

--- pug.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-pug -w ./pug -o ./html -P
--- script/index.js
+++
@@ -1,8 +0,0 @@
-console.log('pringles');
-
-
-let data = fetch('https://api.github.com/users/anjennings/repos')
-  .then(response => response.json())
-  .then(function(data){
-	  console.log(data);
-  });
